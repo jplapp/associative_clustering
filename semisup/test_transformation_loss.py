73e43536ea09713273624d6a6a4444d436576bae
Philip Haeusser <haeusser@cs.tum.edu>
2017-11-05 13:15:37 +0100

    updates to transformation loss test

--- semisup/test_transformation_loss.py
+++ semisup/test_transformation_loss.py
@@ -3,29 +3,56 @@ import numpy as np
 import tensorflow as tf
 
 with tf.Graph().as_default():
-  batch_size = 10
-  emb_size = 128
-  np_embeddings = np.identity(emb_size)[:batch_size] + np.random.uniform(-.2, .2, (batch_size, emb_size))
-  t_embs = tf.Variable(np_embeddings)
-  t_aug_embs_good = t_embs
-  t_aug_embs_bad = tf.Variable(np.random.uniform(-.2, .2, (batch_size, emb_size)))
-  t_embs_logits = tf.slice(t_embs, (0, 0), (batch_size, 10))
-  t_aug_embs_logits = t_embs_logits
+    batch_size = 100
+    emb_size = 128
+    num_classes = 10
 
-  #t_embs_repeated = semisup.backend.tf_repeat(t_embs, [3,1])
+    embeddings = []
+    logits = []
+
+    class_embs = np.identity(emb_size)
+    np.random.shuffle(class_embs)
+    logit_embs = np.identity(num_classes)
+
+    for b in range(batch_size):
+        c = np.random.randint(0, num_classes)
+        emb = class_embs[c] + np.random.uniform(-0.3, 0.3, emb_size)
+        embeddings.append(emb)
+        logit = logit_embs[c] + np.random.uniform(-0.3, 0.3, emb_size)
+        logits.append(logit)
+
+    embeddings = np.array(embeddings)
+    logits = np.array(logits)
+
+    t_embs = tf.Variable(embeddings)
+    t_logits = tf.Variable(logits)
+
+    t_embs_bad = tf.random_normal((batch_size, emb_size))
+    t_logits_bad = tf.random_normal((batch_size, num_classes))
+
+    def dummy(*args, **kwargs):
+        return np.zeros((100, 128))
 
 
-  def dummy(*args, **kwargs):
-    return np.zeros((100, 128))
-  model = semisup.SemisupModel(dummy, 10, [1])
+    model = semisup.SemisupModel(dummy, 10, [1])
 
-  t_loss_good = model.add_transformation_loss(t_embs, t_aug_embs_good, t_embs_logits, t_aug_embs_logits, batch_size)
-  t_loss_bad = model.add_transformation_loss(t_embs, t_aug_embs_bad, t_embs_logits, t_aug_embs_logits, batch_size)
+    t_loss_good = model.add_transformation_loss(t_embs, t_embs,
+                                                t_logits,
+                                                t_logits, batch_size)
+    t_loss_bad_1 = model.add_transformation_loss(t_embs, t_embs_bad,
+                                            t_logits,
+                                            t_logits, batch_size)
+    t_loss_bad_2 = model.add_transformation_loss(t_embs, t_embs,
+                                            t_logits,
+                                            t_logits_bad, batch_size)
 
-  with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    [np_loss_good, np_loss_bad, np_embs] = sess.run([t_loss_good, t_loss_bad, t_embs])
-    print('good loss', np_loss_good)
-    print('bad loss', np_loss_bad)
-    # print('embs', np_embs)
-    assert np_loss_good < np_loss_bad
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        [np_loss_good, np_loss_bad_1, np_loss_bad_2, np_embs] = sess.run(
+            [t_loss_good, t_loss_bad_1, t_loss_bad_2, t_embs])
+        print('good loss', np_loss_good)
+        print('bad loss 1', np_loss_bad_1)
+        print('bad loss 2', np_loss_bad_2)
+        # print('embs', np_embs)
+        assert np_loss_good < np_loss_bad_1
+        assert np_loss_good < np_loss_bad_2
